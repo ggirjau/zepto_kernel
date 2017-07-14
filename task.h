72bcf3bde5d0c3fa4e64f4140468e4506a4ed3a9
ggirjau <gheorghegirjau@gmail.com>
2017-07-14 12:43:09 +0300

    rebuild task module. add child task and dinamyc mem allocation of a new task

---
+++ task.h
@@ -0,0 +1,116 @@
+/*!
+ ******************************************************************************
+ * @File Name          : task.h
+ * @Description        : kernel tasks definition file
+ ******************************************************************************
+ *
+ * COPYRIGHT(c) 2017 Ghirjev Gheorghe
+ *
+ *
+ ******************************************************************************
+ */
+  
+  
+#ifndef __TASK_H
+    #define __TASK_H
+  
+/*!
+ * @Includes
+ */
+#include <stdint.h>
+#include "kernel_sys_cfg.h"
+ 
+/*!
+ * @Defines
+ */
+#define ZEPTO_N_TASKS           (21)
+#define TASK_AMOUNT             (GET_N_ELEMENT_OF(task_list))
+#define GET_N_ELEMENT_OF(a)     (sizeof(a) / sizeof(*a))
+#define PEND_SWITCH_CONTEXT     (1)
+#define CLEAR_SWITCH_CONTEXT    (0)
+ 
+ 
+ 
+ /*
+ *	According to http://www.ugrad.cs.ubc.ca/~cs219/CourseNotes/Threads/threads-states.html
+ *	define Task states.
+ */
+typedef enum 
+{
+    NEW = 0,	//	In this state, the thread is inactive and consumes no CPU cycles.
+	RUNNING,	//	A thread is in the runnable state if it is currently running or if it is ready to run.
+	BLOCKED,	//	A thread is in the blocked state if it cannot continue execution.
+	DEAD		//	A thread is in the dead state if it has completed the code in its run() method or if it terminates abnormally due to an exception.
+} task_state_t;
+
+struct task_node 
+{
+    char * task_name;
+    uint8_t priority : 4;
+    
+    uint16_t running_time;
+    uint16_t offset;
+    uint16_t timer;
+    
+    task_state_t state;                 // NEW, RUNNING...
+    void (*func_p)();                   // call back of a new task.
+    
+    struct task_node * task_child;      // create a task child.
+};
+ 
+typedef struct task_node task_t;
+
+
+/*!
+ * @Functions
+ */
+
+/*!
+ * @brief:		Create task, add new task params to task_t task_list structure used in scheduler.
+ *              Index task_list structure with static task_cnt which should be incremented at each new_task creation.                           
+ *
+ * @param[in]:	running time of a task in ms.
+ * @param[in]:	callback to the new task function.                            
+ * @return: 	none
+ **/
+os_status_t create_task (char * task_name, uint16_t running_time, void (*callback));
+
+/*!
+ * @brief:		Init Scheduler. 
+ *
+ * @param[in]:	none
+ * @return: 	none
+ **/
+void init_scheduler (void);
+
+/*!
+ * @brief:		Check for a context switching. Update time of tasks trough decrementing duration/timer value.
+ *
+ * @param[in]:	none
+ * @return: 	none
+ **/
+void update_timer_task (void);
+
+/*!
+ * @brief:		Start execution of tasks.
+ *
+ * @param[in]:	none
+ * @return: 	none
+ **/
+void start_kernel (void);
+
+/*tasks functions*/
+/* Idle task */
+void idle_task (void);
+
+/* 1ms task */
+void task_1ms (void);
+
+/* 500ms task */
+void task_500ms (void);
+
+/* 1000ms task */
+void task_1000ms (void);
+ 
+#endif // END OF TASK FILE
+
